f91240427986c28af5b7b60235023fb7d0227852
Justin Yurkanin <yurkanin321@gmail.com>
2023-06-18 17:15:41 -0500

    My b, forgot a file

---
+++ src/auvsl_dynamics/generated/model_constants.cpp
@@ -0,0 +1,13 @@
+#include "generated/model_constants.h"
+
+namespace Jackal {
+	namespace rcg {
+		Scalar ix_base_link{0.38783785700798035};
+		Scalar ixy_base_link{0.0011965520679950714};
+		Scalar ixz_base_link{-0.003115505911409855};
+		Scalar iy_base_link{0.46875107288360596};
+		Scalar iyz_base_link{0.0031140821520239115};
+		Scalar iz_base_link{0.4509454071521759};
+		Scalar m_base_link{16.52400016784668};
+	}
+}
